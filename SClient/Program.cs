261929833644a752c7b87405593c37c3a4c7296f
shawn98xw <shawn98xw@gmail.com>
2022-06-12 16:04:34 +0800

    client

---
+++ SClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using SocketUtil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace SocketClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SocketClient client = new SocketClient(8888);
+            client.StartClient();
+            Console.ReadKey();
+        }
+    }
+}
